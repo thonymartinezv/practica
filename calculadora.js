f4efee29e4dd350494a75e3222ddbdc18ec93999
thonymartinezv <anthony.mart996@gmail.com>
2017-03-08 01:18:11 -0400

    Update calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -11,14 +11,14 @@
 	//función para escribir con el teclado
 	function imprimir(num){
 		var midisplay=document.getElementById("display");//display: input donde se vera cifra numérica ingresada
-		midisplay.value=cifra+num;
-		cifra=midisplay.value;
+		midisplay.value=cifra+num;//generando la cifra concatenado los numeros ingresados
+		cifra=midisplay.value;//Imprimiendo en el input la cifra actual
 	}
 	//Esta función borra el ultimo número escrito
 	function borrar(){
 		var midisplay=document.getElementById("display");//display: input donde se vera cifra numérica ingresada
-		cifra=midisplay.value;
-		midisplay.value=cifra.substr(0,cifra.length-1);
+		cifra=midisplay.value;//se asigna a cifra el valor actual de el input
+		midisplay.value=cifra.substr(0,cifra.length-1);//Borrando el último valor de la cifra ingresado
 	}
 	//Esta función reinicia o reestablece los valores de la calculadora
 	function borrar_todo(){
@@ -37,47 +37,55 @@
 	function resultado(){
 		var midisplay=document.getElementById("display");//display: input donde se vera cifra numérica ingresada
 		var midisplay2=document.getElementById("display2");//display2:input donde se vera el resultado aculmulado
-		num2=midisplay.value;
-		if((num2==""&&operador=="+")||(num2==""&&operador=="-")){num2="0"}
-		else if((num2==""&&operador=="*")||(num2==""&&operador=="/")){num2="1"}else{}
+		num2=midisplay.value;//se almacena el valor ingresado para realizar la operación
+		//los siguientes if son para neutralizar las operaciones carente de numeros ingresados con el objetivo de no alterar el resultado almacendo
+		if((num2==""&&operador=="+")||(num2==""&&operador=="-")){num2="0"}//Si el usuario no a ingresado numeros utiliza el elemento neutro de la suma y resta "0"
+		else if((num2==""&&operador=="*")||(num2==""&&operador=="/")){num2="1"}else{}//Si el usuario no a ingresado numeros utiliza el elemento neutro de la multiplicación "1"
+		//El siguiente switch evalua la operación a realizar
 		switch(operador){
+				/*Los siguientes if anidados en el switch son para neutralizar operaciones hasta que
+			 	  el usuario ingrese almenos dos numeros*/
 			case "+":
+				//En caso de que no haya resultado almacenado asigname el elemento neutro de la suma
 				if(num1==""){num1="0"}else{}
-				num1= parseFloat(num1)+parseFloat(num2);
+				num1= parseFloat(num1)+parseFloat(num2);//suma
 			break;
 			case "-":
+				//En caso de que no haya resultado almacenado asigname un numero que al restarle el ingresado de igual 
 				if(num1==""){num1=(parseFloat(num2)*2)}else{}
-				num1= parseFloat(num1)-parseFloat(num2);
+				num1= parseFloat(num1)-parseFloat(num2);//resta
 			break;
 			case "*":
+				//En caso de que no haya resultado almacenado asigname el numero neutro de la multiplicación
 				if(num1==""){num1="1"}else{}
-				num1= parseFloat(num1)*parseFloat(num2);
+				num1= parseFloat(num1)*parseFloat(num2);//multiplicación
 			break;
 			case "/":
-				if(num1==""){num1=(parseFloat(num2)*parseFloat(num2))}else{}
-				num1= parseFloat(num1)/parseFloat(num2);
+				//En caso de que no haya resultado almacenado asigname un numero que al dividirlo entre el ingresado de igual
+				if(num1==""){num1=(parseFloat(num2)*parseFloat(num2))}else{}				
+				num1= parseFloat(num1)/parseFloat(num2);//división
 			break;
 			default:
-				alert("inserte una operación válida");
+				alert("inserte una operación válida");//En caso de que el operador no haya sido ingresado
 			break;
 		}
-		midisplay2.value=num1;
-		midisplay.value="";
-		cifra="";
+		midisplay2.value=num1;//se imprime el resultado en el input donde se pretende ver
+		midisplay.value="";//vaciamos el imput donde se ve la cifra ingresada
+		cifra="";//reiniciamos la cifra
 	}
 	//Esta se ejecuta al pulsar el signo igual
 	function resultado_final(){
-		resultado();
-		if(num1!=""){alert(num1);}
+		resultado();//Se calcula la operación
+		if(num1!=""){alert(num1);}//Si existe un resultado imprimelo en una alerta
 	}
 	//Esta función se ejecutara cada vez que el usuario seleccione un signo de operación donde lo recibira como parametro
 	function operacion(signo){
 		var midisplay=document.getElementById("display");//display: input donde se vera cifra numérica ingresada
 		var imprimir_signo=document.getElementById("operador");//operador:input donde se vera la operacion a realizar
-		if(operador==""){operador=signo;}else{}
-		resultado();
-		operador=signo;
-		imprimir_signo.value=operador;
+		if(operador==""){operador=signo;}else{}//Para evitar errores en caso de que no haya signo asignado en el input
+		resultado();//Se calcula la operación actual
+		operador=signo;//guardando signo de operación
+		imprimir_signo.value=operador;//imprimiendo signo de operación en el input asignado
 		
 	}
 	//esta función no tiene importancia solo es de prueba y se acciona como evento de el boton P
